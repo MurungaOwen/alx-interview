a2b209de647dbab27e2a43a274ff1964bb96efec
MurungaOwen <owenhood80@gmail.com>
2024-03-07 22:22:06 +0300

    handle user input

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,18 @@
 def pascal_triangle(n):
-    if n <= 0:
-        return []
+    try:
+        n = int(n)
+        if n <= 0:
+            return []
 
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1] #initialise a row with 1 as first value for each row
-        print(f"i is {i}")
-        for j in range(1, i):#loop for values inside the row
-            print(f"j is at {triangle[i - 1]}")
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-    return triangle
+        triangle = [[1]]
+        for i in range(1, n):
+            row = [1] #initialise a row with 1 as first value for each row
+            for j in range(1, i):#loop for values inside the row
+                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(value)
+            row.append(1)
+            triangle.append(row)
+        return triangle
+    except ValueError :
+        print("Invalid input: n must be an integer.")
+        return []
