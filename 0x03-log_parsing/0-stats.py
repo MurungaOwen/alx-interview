f0647bbcf788d7063b1a68dde0bb1a3a3c1317e2
MurungaOwen <owenhood80@gmail.com>
2024-03-27 01:00:04 +0300

    lint

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,31 +1,42 @@
+#!/usr/bin/python3
+"""log parsing"""
 import sys
 import re
 import signal
 
+
 def validate_line(line):
+    """validate our ip address"""
     pattern = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] \"GET \/projects\/260 HTTP\/1\.1\" (\d+) (\d+)"
     matching = re.match(pattern, line)
     if matching:
         status_code = matching.group(2)
         file_size = int(matching.group(3))
-        return [status_code,file_size]
+        return [status_code, file_size]
     return None
 
 def print_metrics(status, f_size):
+    """print outpout"""
     print(f"File size: {f_size}") 
     for key, value in status.items():
         if value > 0 :
             print(f"{key}: {value}")
 
 def signal_handler(sig, frame):
+    """signalling when ctrl+c"""
     print_metrics(status_codes, total_file_size)
     sys.exit(0)
 
 
 def main():
+    """opening function"""
     line_done = 0
     global status_codes, total_file_size
-    status_codes = {"200": 0, "301": 0, "400" : 0, "401" : 0, "403": 0, "404": 0, "405": 0,"500": 0}
+    status_codes = {
+        "200": 0, "301": 0, "400" : 0,
+        "401" : 0, "403": 0, "404": 0,
+        "405": 0,"500": 0
+        }
     total_file_size = 0
     signal.signal(signal.SIGINT, signal_handler)
     try:
