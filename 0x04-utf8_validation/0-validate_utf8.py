0671294e6a274a2d8a1e366a91f4373695c32295
MurungaOwen <owenhood80@gmail.com>
2024-04-03 13:14:33 +0300

    rm inline comments

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,15 +16,14 @@ def validUTF8(data):
 
     for byte in data:
         if bytes_for_following == 0:
-            if byte >> 5 == 0b110:  # 2byte
+            if byte >> 5 == 0b110:
                 bytes_for_following = 1
-            elif byte >> 4 == 0b1110:  # 3byte
+            elif byte >> 4 == 0b1110:
                 bytes_for_following = 2
-            elif byte >> 3 == 0b11110:  # 4 byte
+            elif byte >> 3 == 0b11110:
                 bytes_for_following = 3
         else:
-            if byte >> 6 != 0b10:  # check for next item in list
+            if byte >> 6 != 0b10:
                 return False
             bytes_for_following -= 1
-            
     return True
